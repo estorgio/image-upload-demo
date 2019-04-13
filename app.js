7cb2ffa26830755c8055400500a3c92479637aba
Fortunato Estorgio <contact@estorgio.com>
2019-04-14 06:35:16 +0800

    Change Cache-Control headers for static files

--- app.js
+++ app.js
@@ -23,8 +23,14 @@ app.use(expressSession({
 }));
 
 app.set('view engine', 'ejs');
-app.use('/static', express.static(path.join(__dirname, 'public')));
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+app.use('/static/vendor',
+  express.static(path.join(__dirname, 'public', 'vendor'),
+    { immutable: true, maxAge: '1y' }));
+app.use('/static/assets',
+  express.static(path.join(__dirname, 'public', 'assets'),
+    { maxAge: '1d' }));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(flash());
 app.use((req, res, next) => {
